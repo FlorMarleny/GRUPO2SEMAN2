1622dd15930c1171b23106cba1f2331f420eca9e
Diexmania1992 <diexalejandro@gmail.com>
2026-04-04 17:26:12 -0500

    DÑJ_INICIO

--- GRUPO2SEMAN2/Program.cs
+++ GRUPO2SEMAN2/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int saldo = 1000;
+int monto;
+string continuar = " ";
+
+do
+{
+    Console.WriteLine("Ingrese el monto a retirar:");
